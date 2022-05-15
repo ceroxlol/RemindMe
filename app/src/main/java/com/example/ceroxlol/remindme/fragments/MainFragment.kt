46f490ebf510865e06af59ed89418233c48918bc
Christopher Werner <christopher.werner@netlight.com>
2022-05-15 21:31:10 +0200

    Change MainFragment to contain appointments and add button handler to add new appointment.

--- app/src/main/java/com/example/ceroxlol/remindme/fragments/MainFragment.kt
+++ app/src/main/java/com/example/ceroxlol/remindme/fragments/MainFragment.kt
@@ -7,8 +7,10 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.ceroxlol.remindme.RemindMeApplication
-import com.example.ceroxlol.remindme.adapters.ArrayAdapterAppointments
+import com.example.ceroxlol.remindme.adapters.AppointmentListAdapter
+import com.example.ceroxlol.remindme.databinding.AppointmentListFragmentBinding
 import com.example.ceroxlol.remindme.models.viewmodel.AppointmentKTViewModel
 import com.example.ceroxlol.remindme.models.viewmodel.AppointmentKTViewModelFactory
 
@@ -16,25 +18,42 @@ class MainFragment : Fragment() {
 
     private val viewModel: AppointmentKTViewModel by activityViewModels {
         AppointmentKTViewModelFactory(
-            (activity?.application as RemindMeApplication).database
-                .appointmentDao()
+            (activity?.application as RemindMeApplication).database.appointmentDao()
         )
     }
 
+    private var _binding: AppointmentListFragmentBinding? = null
+    private val binding get() = _binding!!
+
+
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
+        inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+    ): View {
+        _binding = AppointmentListFragmentBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val adapter = ArrayAdapterAppointments {
+        val adapter = AppointmentListAdapter {
             val action =
-                ItemListFragmentDirections.actionItemListFragmentToItemDetailFragment(it.id)
+                MainFragmentDirections.actionMainFragmentToAddAppointmentFragment()
+            this.findNavController().navigate(action)
+        }
+        binding.recyclerView.layoutManager = LinearLayoutManager(this.context)
+        binding.recyclerView.adapter = adapter
+        // Attach an observer on the allItems list to update the UI automatically when the data
+        // changes.
+        viewModel.allItems.observe(this.viewLifecycleOwner) { items ->
+            items.let {
+                adapter.submitList(it)
+            }
+        }
+
+        binding.floatingActionButton.setOnClickListener {
+            val action = AddNewAppointmentFragmentKTDirections.actionAddAppointmentFragmentToMainFragment()
             this.findNavController().navigate(action)
         }
     }
