b29c2bbd81f35210bbdbc9880897e18a63c51790
Christopher Werner <christopher.werner@netlight.com>
2022-06-12 10:35:18 +0200

    I have no idea... :(

---
+++ app/src/main/java/com/example/ceroxlol/remindme/fragments/ScreenSlidePageFragment.kt
@@ -0,0 +1,17 @@
+package com.example.ceroxlol.remindme.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.ceroxlol.remindme.R
+
+class ScreenSlidePageFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View = inflater.inflate(R.layout.fragment_screen_slide_page, container, false)
+}
