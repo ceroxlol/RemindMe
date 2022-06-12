c252969b991a100b5624168ddc8d8388a1f6ceb6
Christopher Werner <christopher.werner@netlight.com>
2022-06-12 10:32:55 +0200

    Add second constructor to DbLocation class, so it can be directly constructed from latLong, as this is more handy working with android.Location.

--- app/src/main/java/com/example/ceroxlol/remindme/models/LocationMarker.kt
+++ app/src/main/java/com/example/ceroxlol/remindme/models/LocationMarker.kt
@@ -4,6 +4,7 @@ import androidx.room.ColumnInfo
 import androidx.room.Embedded
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.google.android.gms.maps.model.LatLng
 
 @Entity
 data class LocationMarker(
@@ -19,4 +20,6 @@ data class LocationMarker(
 data class DbLocation(
     val latitude: Double = 0.0,
     val longitude: Double = 0.0
-)
+) {
+    constructor(latLong: LatLng) : this(latitude = latLong.latitude, longitude = latLong.longitude)
+}
