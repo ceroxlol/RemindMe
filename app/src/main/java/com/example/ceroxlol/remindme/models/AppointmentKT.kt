610a76a9d902c7a1c314c30ae0b122944d361344
Christopher Werner <christopher.werner1@gmx.net>
2022-06-15 18:08:51 +0200

    Cleanup AppointmentKT.kt

--- app/src/main/java/com/example/ceroxlol/remindme/models/AppointmentKT.kt
+++ app/src/main/java/com/example/ceroxlol/remindme/models/AppointmentKT.kt
@@ -4,7 +4,6 @@ import androidx.room.ColumnInfo
 import androidx.room.Embedded
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import lombok.Builder
 import java.util.*
 
 @Entity
@@ -17,35 +16,3 @@ data class AppointmentKT(
     @ColumnInfo val time: Date?,
     @ColumnInfo val done: Boolean
 )
-
-/*
-    public static final Creator<Appointment> CREATOR = new Creator<Appointment>() {
-        @Override
-        public Appointment createFromParcel(Parcel in) {
-            return new Appointment(in);
-        }
-
-        @Override
-        public Appointment[] newArray(int size) {
-            return new Appointment[size];
-        }
-    };
-
-    //TODO: Implement Parcelable
-    @Override
-    public int describeContents() {
-        return 0;
-    }
-
-    @Override
-    public void writeToParcel(Parcel dest, int flags) {
-        dest.writeInt(id);
-        dest.writeString(name);
-        dest.writeString(appointmentText);
-        dest.writeParcelable(location, flags);
-        dest.writeByte((byte) (hasTime == null ? 0 : hasTime ? 1 : 2));
-        dest.writeInt(priority);
-        dest.writeInt(type);
-        dest.writeByte((byte) (isActive == null ? 0 : isActive ? 1 : 2));
-    }
- */
