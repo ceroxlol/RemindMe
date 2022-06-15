55a91f2f14d314ff4b78aaafa4ae1d5b33a3041e
Christopher Werner <christopher.werner1@gmx.net>
2022-06-15 18:07:56 +0200

    Extend mapper for toLatLng for my DbLocation object.

--- app/src/main/java/com/example/ceroxlol/remindme/utils/Mapper.kt
+++ app/src/main/java/com/example/ceroxlol/remindme/utils/Mapper.kt
@@ -1,8 +1,13 @@
 package com.example.ceroxlol.remindme.utils
 
 import android.location.Location
+import com.example.ceroxlol.remindme.models.DbLocation
 import com.google.android.gms.maps.model.LatLng
 
 fun Location.toLatLng() : LatLng {
+    return LatLng(this.latitude, this.longitude)
+}
+
+fun DbLocation.toLatLng() : LatLng{
     return LatLng(this.latitude, this.longitude)
 }
