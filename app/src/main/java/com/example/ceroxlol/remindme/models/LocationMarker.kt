475456d1aca53c0a35d24e6a0c0f42f249b1f6bb
Christopher Werner <christopher.werner1@gmx.net>
2022-07-05 08:53:54 +0200

    Extend LocationMarker for an Address.

--- app/src/main/java/com/example/ceroxlol/remindme/models/LocationMarker.kt
+++ app/src/main/java/com/example/ceroxlol/remindme/models/LocationMarker.kt
@@ -1,15 +1,18 @@
 package com.example.ceroxlol.remindme.models
 
+import android.location.Address
 import androidx.room.ColumnInfo
 import androidx.room.Embedded
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import java.util.*
 
 @Entity
 data class LocationMarker(
     @ColumnInfo(name = "location_id") @PrimaryKey(autoGenerate = true) val id: Int = 0,
     @Embedded val location: DbLocation,
-    @ColumnInfo(name = "location_name") val name: String
+    @ColumnInfo(name = "location_name") val name: String,
+    @Embedded val address: Address? = null
 ) {
     fun isValid(): Boolean {
         return name.isNotEmpty()
