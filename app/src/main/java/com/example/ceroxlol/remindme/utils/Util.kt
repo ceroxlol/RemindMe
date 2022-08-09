edf51e55b013276bfd5ccb07e6c485ca781ce55c
Christopher Werner <christopher.werner@netlight.com>
2022-08-09 17:48:38 +0200

    WIP of integrating a foreground service.

---
+++ app/src/main/java/com/example/ceroxlol/remindme/utils/Util.kt
@@ -0,0 +1,44 @@
+package com.example.ceroxlol.remindme.utils
+
+import android.app.Activity
+import android.app.ActivityManager
+import android.content.Context
+import android.content.Intent
+import android.location.LocationManager
+import android.provider.Settings
+import android.util.Log
+import androidx.appcompat.app.AlertDialog
+
+object Util {
+    fun isMyServiceRunning(serviceClass: Class<*>, mActivity: Activity): Boolean {
+        val manager: ActivityManager =
+            mActivity.getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
+        for (service in manager.getRunningServices(Int.MAX_VALUE)) {
+            if (serviceClass.name == service.service.className) {
+                Log.i("Service status", "Running")
+                return true
+            }
+        }
+        Log.i("Service status", "Not running")
+        return false
+    }
+
+    fun isLocationEnabledOrNot(context: Context): Boolean {
+        val locationManager =
+            context.getSystemService(Context.LOCATION_SERVICE) as LocationManager?
+        return locationManager!!.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
+            LocationManager.NETWORK_PROVIDER
+        )
+    }
+
+    fun showAlertLocation(context: Context, title: String, message: String, btnText: String) {
+        val alertDialog = AlertDialog.Builder(context).create()
+        alertDialog.setTitle(title)
+        alertDialog.setMessage(message)
+        alertDialog.setButton(android.R.id.button1, btnText) { dialog, _ ->
+            dialog.dismiss()
+            context.startActivity(Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS))
+        }
+        alertDialog.show()
+    }
+}
