e3b26ee8f9fbf12e6df3557861d85a502fa7111f
Christopher Werner <christopher.werner@netlight.com>
2022-09-28 17:40:37 +0200

    Add view model for AppointmentAndLocationMarker

--- app/src/main/java/com/example/ceroxlol/remindme/models/viewmodel/AppointmentAndLocationMarkerViewModel.kt
+++ app/src/main/java/com/example/ceroxlol/remindme/models/viewmodel/AppointmentAndLocationMarkerViewModel.kt
@@ -3,13 +3,21 @@ package com.example.ceroxlol.remindme.models.viewmodel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
 import com.example.ceroxlol.remindme.models.dao.AppointmentDao
+import kotlinx.coroutines.launch
 
-class AppointmentAndLocationMarkerViewModel(val appointmentDao: AppointmentDao) :
+class AppointmentAndLocationMarkerViewModel(private val appointmentDao: AppointmentDao) :
     ViewModel() {
 
     val getAllSortedByLocationMarkerId =
         appointmentDao.getAppointmentAndLocationMarkerSortedByLocationMarkerId().asLiveData()
+
+    fun deleteLocationMarkerForAppointments(locationMarkerId: Int) {
+        viewModelScope.launch {
+            appointmentDao.setLocationMarkerDeleted(locationMarkerId)
+        }
+    }
 }
 
 class AppointmentAndLocationMarkerViewModelFactory(private val appointmentDao: AppointmentDao) :
